4f12793c41499523c65d71e8cdfb3b186368c4f8
Kevin McCarthy <mccarthyk@hcflgov.net>
2016-12-27 11:07:57 -0500

    init highchairs

--- app/helpers/child_fatalities_helper.rb
+++ app/helpers/child_fatalities_helper.rb
@@ -1,9 +1,7 @@
 module ChildFatalitiesHelper
 
 	def jumper(column, options={})
-
 		return year_jumper if column == :year
-
 		options = ChildFatality.all.map{ |record|
 			jump_url = url_for(params.except(:action, :controller, column.to_sym).merge(column => record.send(column)).permit!)
 			[
@@ -12,14 +10,11 @@ module ChildFatalitiesHelper
 				data: {jumper: jump_url}
 			]
 		}.uniq.sort
-
 		options.unshift [column.capitalize, nil, data: {jumper: url_for(params: params.except(:action, :controller, column).to_h)}]
-
 		select_tag column, options_for_select( options, params[column] ), class: 'jumper-select form-control'
 	end
 
 	def year_jumper
-
 		options = ChildFatality.all.map{ |record|
 			jump_url = url_for(params.except(:action, :controller, :year).merge(year: record.dod.year).permit!)
 			[
@@ -28,11 +23,8 @@ module ChildFatalitiesHelper
 				data: {jumper: jump_url}
 			]
 		}.uniq.sort.reverse
-
 		options.unshift ['Year', nil, data: {jumper: url_for(params: params.except(:action, :controller, :year).to_h)}]
-
 		select_tag 'year', options_for_select( options, params[:year] ), class: 'jumper-select form-control'
 	end
-
 
 end
