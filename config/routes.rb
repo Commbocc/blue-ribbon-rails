fc61d14fadcaa204bebe749149a22f98847fc43e
Kevin McCarthy <mccarthyk@hcflgov.net>
2017-01-12 10:55:40 -0500

    root to external url

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :partners, only: [:index, :show]
   resources :child_fatalities, only: [:index, :show], path: 'facts-figures'
 
-  root 'child_fatalities#index'
+  root to: redirect('http://hcflgov.net/en/government/boards-and-committees/a-d/blue-ribbon-committee-for-child-safety')
 end
